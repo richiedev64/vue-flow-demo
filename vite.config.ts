64dd6832bb9118d33bced1273b2ce0763d8ecbb8
richiedev <60094056+richiedev64@users.noreply.github.com>
2026-08-25 04:26:29 +0000

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~': resolve('src'),
+    },
+    dedupe: ['vue'],
+    extensions: ['.ts', '.vue'],
+  },
+  plugins: [vue()],
+})
